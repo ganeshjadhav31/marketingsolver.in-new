5be00a6fd959bb1702ec6e4f750bbfcd2c27d081
ganeshjadhav31 <ganeshmarketing2410@gmail.com>
2025-01-18 12:08:07 +0530

    add tailwind change in about page

--- website/app/about/page.js
+++ website/app/about/page.js
@@ -3,7 +3,13 @@ import React from 'react'
 const about = () => {
   return (
     <div>
-        Hello about page
+        <h1 className="text-xl">About Us</h1>
+        <ul>
+            <li>Digital Marketing Agency</li>
+            <li>MarketingSolver.in</li>
+            <li>Web Development</li>
+            <li>Web Development</li>
+        </ul>
     </div>
   )
 }
